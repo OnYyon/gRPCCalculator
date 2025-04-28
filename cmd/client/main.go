8352d252c2917449804614e3ef184cc71ff55217
OnYyon <oleg.school@mai.ru>
2025-04-28 21:40:36 +0300

    Skelet for orchestrator <-> agent(gRPC)

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,30 +3,49 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
+	"io"
 
 	proto "github.com/OnYyon/gRPCCalculator/proto/gen"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+type quequeTask struct {
+	queque chan *proto.Task
+}
+
 // For tests
+// Я думаю жто кастыль, но он работает
+// TODO: Попробыть способ Unary и по изучать еще как это реализоватья
 func main() {
+	numWorkers := 3
+	quequeTask := quequeTask{make(chan *proto.Task, numWorkers)}
+	for i := 0; i < numWorkers; i++ {
+		go quequeTask.StartAgent(i)
+	}
 	conn, err := grpc.NewClient("localhost:8080",
 		grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		panic("error!")
+		panic("erorr!")
 	}
 	defer conn.Close()
-
 	grpcClient := proto.NewOrchestratorClient(conn)
 	stream, err := grpcClient.TransportTasks(context.Background())
 	if err != nil {
-		log.Fatalf("failed to create stream: %v", err)
+		panic("error in client/main.go")
 	}
-	task, err := stream.Recv()
-	if err != nil {
-		panic("error!")
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		go func() { quequeTask.queque <- req }()
 	}
-	fmt.Printf("get: %v", task)
+}
+
+func (q *quequeTask) StartAgent(num int) {
+	task := <-q.queque
+	fmt.Println(num, task)
+
 }
--- iternal/grpc/orchestrator/server.go
+++ iternal/grpc/orchestrator/server.go
@@ -1,14 +1,13 @@
 package orchestrator
 
 import (
-	"fmt"
-
 	proto "github.com/OnYyon/gRPCCalculator/proto/gen"
 	"google.golang.org/grpc"
 )
 
 type serverAPI struct {
 	proto.UnimplementedOrchestratorServer
+	queque chan *proto.Task
 }
 
 func RegisterOrchestratorServer(gRPC *grpc.Server) {
@@ -19,16 +18,34 @@ func RegisterOrchestratorServer(gRPC *grpc.Server) {
 func (s *serverAPI) TransportTasks(
 	stream grpc.BidiStreamingServer[proto.Task, proto.Task],
 ) error {
-	task := &proto.Task{
-		ID:           "1",
-		Arg1:         "2",
-		Arg2:         "2",
-		Operation:    "+",
-		ExpressionID: "1",
+	sliceTests := []*proto.Task{
+		{
+			ID:           "1",
+			Arg1:         "2",
+			Arg2:         "2",
+			Operation:    "+",
+			ExpressionID: "1",
+		},
+		{
+			ID:           "2",
+			Arg1:         "3",
+			Arg2:         "3",
+			Operation:    "+",
+			ExpressionID: "2",
+		},
+		{
+			ID:           "3",
+			Arg1:         "4",
+			Arg2:         "4",
+			Operation:    "+",
+			ExpressionID: "3",
+		},
 	}
-	if err := stream.Send(task); err != nil {
-		panic("error!")
+
+	for _, task := range sliceTests {
+		if err := stream.Send(task); err != nil {
+			return err
+		}
 	}
-	fmt.Printf("recieve %v", task)
 	return nil
 }
